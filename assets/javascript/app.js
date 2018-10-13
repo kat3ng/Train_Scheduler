c99ea6e9188ff02662122f62b4c2e2704057063f
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-10-12 22:18:47 -0500

    input functionality works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,42 @@
-// Make sure that your app suits this basic spec:
 
-// When adding trains, administrators should be able to submit the following:
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC97u_SJy_E6_vpJ9PuuB6KLzMv9TEKfmE",
+    authDomain: "ke-train-scheduler.firebaseapp.com",
+    databaseURL: "https://ke-train-scheduler.firebaseio.com",
+    projectId: "ke-train-scheduler",
+    storageBucket: "ke-train-scheduler.appspot.com",
+    messagingSenderId: "581787075295"
+};
+firebase.initializeApp(config);
 
-// Train Name
+//
+var database = firebase.database();
 
-// Destination
+$("button").on("click", function (event) {
+    event.preventDefault();
+    var name = $("#train-name").val();
+    var destination = $("#destination-input").val();
+    var start = $("#start-input").val();
+    var frequency = $("#frequency-input").val();
 
-// First Train Time-- in military time
-
-// Frequency-- in minutes
-
-// Code this app to calculate when the next train will arrive; this should be relative to the current time.
-
-// Users from many different machines must be able to view same train times.
-
-// Styling and theme are completely up to you.Get Creative!
+    database.ref().push({
+        "name": name,
+        "destination": destination,
+        "start": start,
+        "frequency": frequency
+    });
+    // find table and add a table row
+    $("#train-time-table tbody").append(
+        `
+<tr>
+    <td>${name}</td>
+    <td>${destination}</td>
+    <td>${start}</td>
+    <td></td>
+    <td>${frequency}</td>
+    <td></td>
+</tr>
+`
+    );
+});
